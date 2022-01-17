395c62925fe07a3496da76a97cfa976715594559
g1ennice <93643326+g1ennice@users.noreply.github.com>
2022-01-17 15:48:47 +0800

    Add files via upload

---
+++ Exercise4.java
@@ -0,0 +1,11 @@
+public class Exercise4 {
+    public static void main(String args[])
+    {
+        for(int x = 101; x > 66 ; x = x - 2){
+            System.out.print("Value of x: " + x);
+            System.out.print("\n");
+        }
+    }
+}
+
+
